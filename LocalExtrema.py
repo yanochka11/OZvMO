c55a6689f60b35b379f540ec32890416bd1935b2
adelinatsoi <dlnxxts@gmail.com>
2022-02-20 23:14:11 +0300

    добавлен метод лагранжа

--- LocalExtrema.py
+++ LocalExtrema.py
@@ -1,3 +1,7 @@
+import numpy as np
+import sympy as sm
+
+
 def local_extrema(variab, func, restr):
     '''
     Функцция поиска локальных экстремумов функции двух переменных
@@ -5,7 +9,8 @@ def local_extrema(variab, func, restr):
     :param func: sympy expression
     :param restr: возвращает словарь, где ключом выступают названия переменных,
     значениями являются списки, с ограничивающими интервалами
-    :return: Список координат и значение функции в точке, для всех точек локальных экстремумов, сц указанием типа экстремума (минимум, максимум, седловая точка,)
+    :return: Список координат и значение функции в точке, для всех точек локальных экстремумов,
+     с указанием типа экстремума (минимум, максимум, седловая точка)
     '''
     x, y = sm.symbols('x, y', real=True)
 
@@ -40,5 +45,61 @@ def local_extrema(variab, func, restr):
 
     output_str = ''
     for i in range(len(sol)):
-        output_str += f'({float(sol[i][x]):.3},\t{float(sol[i][y]):.3},\t{float(sol[i]["func_value"]):.3}) - {sol[i]["type"]} \n'
+        output_str += f'({float(sol[i][x]):.3},\t{float(sol[i][y]):.3},' + \
+                      f'\t{float(sol[i]["func_value"]):.3}) - {sol[i]["type"]} \n'
     return output_str
+
+
+def lagrange(variab, func, restr, restr_func):
+    '''
+    Поиск локальных экстремумов функции двух переменных с ограничениями (метод Лагранжа);
+
+    :param variab: кортеж состоящий из переменных, которые являются sympy symbols
+    :param func: sympy expression
+    :param restr: возвращает словарь, где ключом выступают названия переменных,
+    значениями являются списки, с ограничивающими интервалами
+    :param restr_func: sympy expression
+    :return: Список координат и значение функции в точке, для всех точек локальных экстремумов,
+     с указанием типа экстремума (минимум, максимум, седловая точка)
+    '''
+    x, y, lam = sm.symbols('x, y lambda', real=True)
+
+    func = func.subs({variab[0]: x, variab[1]: y})  # заменяем переменные во избежание комплексных ответов
+    restr_func = restr_func.subs({variab[0]: x, variab[1]: y})
+    lagr_func = func + restr_func*lam
+    lagr_func
+
+    func = func.subs({variab[0]: x, variab[1]: y})  # заменяем переменные во избежание комплексных ответов
+    lagr_func = func + restr_func*lam  # составляем функцию лагранжа
+    restr_func = restr_func.subs({variab[0]: x, variab[1]: y})
+    variab1_d = sm.diff(lagr_func, x)  # дифференцируем по каждой из переменных
+    variab2_d = sm.diff(lagr_func, y)
+
+    sol = sm.solve([variab1_d,
+                    variab2_d,
+                    restr_func],
+                   [x, y, lam], dict=True)  # приравниваем частные производные и ограничивающую функцию к нулю и
+# решаем систему
+
+    sol_restr = []
+    for solution in sol:
+
+        if restr[variab[0]][0] <= solution[x] <= restr[variab[0]][1] and \
+           restr[variab[1]][0] <= solution[y] <= restr[variab[1]][1]:
+            sol_restr.append(solution)
+
+    hessian_matrix = sm.hessian(lagr_func, [lam, x, y])  # Hessian matrix
+
+    for dot in sol_restr:
+        if hessian_matrix.subs(dot).det() > 0:  # смотрим знак определителя матрицы
+            dot['type'] = 'max'  # добавляем в словарь с соответствующей точкой ее тип
+        elif hessian_matrix.subs(dot).det() < 0:  # смотрим знак определителя матрицы
+            dot['type'] = 'min'  # добавляем в словарь с соответствующей точкой ее тип
+        else:
+            dot['type'] = 'saddle'  # добавляем в словарь с соответствующей точкой ее тип
+        dot['func_value'] = func.subs(dot)
+
+    output_str = ''
+    for i in range(len(sol_restr)):
+        output_str += f'({float(sol_restr[i][x]):.3}, {float(sol_restr[i][y]):.3}, ' +\
+                    f'{float(sol_restr[i]["func_value"]):.3}) - {sol_restr[i]["type"]} \n'
