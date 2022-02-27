b370bdc7e74f99eefb6b611efc7cf9eb44d6d8be
adelinatsoi <dlnxxts@gmail.com>
2022-02-27 15:29:22 +0300

    вариант для двух заданий

--- task1.py
+++ task1.py
@@ -3,11 +3,17 @@ import OZvMO.LocalExtrema as LocalExtrema
 import OZvMO.plot as plot
 
 
-def task1():
+def solve_task1():
     variab, func, restr = parser.parser_task1()
     output_str, dots = LocalExtrema.local_extrema(variab, func, restr)
     fig = plot.plot(variab, func, restr, dots)
+    print(output_str)
+    fig.show()
 
-    return output_str, fig
 
-
+def solve_task2()
+    variables, func, restr, restr_func = parser.parser_task2()
+    output_str, sol = lagrange()
+    fig = plot.plot(variab, func, restr, dots)
+    print(output_str)
+    fig.show()
