b07adbb07b019d9be312964bdfc3e5a14fa49628
Adelina <99534328+tsoiadelina@users.noreply.github.com>
2022-03-04 23:13:01 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,15 +5,15 @@ import OZvMO.plot as plot
 
 def solve_task1():
     variab, func, restr = parser.parser_task1()
-    output_str, dots = LocalExtrema.local_extrema(variab, func, restr)
-    fig = plot.plot(variab, func, restr, dots)
-    print(output_str)
+    sol_restr_all, sol_restr = LocalExtrema.local_extrema(variab, func, restr)
+    fig = plot.plot(variab, func, restr, sol_restr)
+    print(sol_restr_all)
     fig.show()
 
 
 def solve_task2():
     variables, func, restr, restr_func = parser.parser_task2()
-    output_str, dots = LocalExtrema.lagrange(variables, func, restr, restr_func)
-    fig = plot.plot(variables, func, restr, dots)
-    print(output_str)
+    sol_restr_all, sol_restr = LocalExtrema.lagrange(variables, func, restr, restr_func)
+    fig = plot.plot(variables, func, restr, sol_restr)
+    print(sol_restr_all)
     fig.show()
