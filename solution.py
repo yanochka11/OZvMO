0a9e0efeb0f8415db960d6aaf2529252e0da5a34
Adelina <99534328+tsoiadelina@users.noreply.github.com>
2022-03-05 00:49:30 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,8 +6,8 @@ from IPython.display import display
 def solve_task1():
     variab, func, restr = parser.parser_task1()
     sol_restr_all, sol_restr = LocalExtrema.local_extrema(variab, func, restr)
-#     if sol_restr_all == []:
-#         sol_restr_all = 'нет точек экстремума'
+    if sol_restr_all is []:
+         sol_restr_all = 'нет точек экстремума'
     fig = plot.plot(variab, func, restr, sol_restr)
     display(sol_restr_all)
     fig.show()
@@ -16,8 +16,8 @@ def solve_task1():
 def solve_task2():
     variables, func, restr, restr_func = parser.parser_task2()
     sol_restr_all, sol_restr = LocalExtrema.lagrange(variables, func, restr, restr_func)
-#     if sol_restr_all == []:
-#         sol_restr_all = 'нет точек экстремума'
+     if sol_restr_all is []:
+         sol_restr_all = 'нет точек экстремума'
     fig = plot.plot(variables, func, restr, sol_restr)
     display(sol_restr_all)
     fig.show()
