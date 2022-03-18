b2c57bde18e344bb60de77d378848a499722fe39
adelinatsoi <dlnxxts@gmail.com>
2022-03-18 13:28:28 +0300

    функция для золотого сечения

---
+++ TZ2/GoldenRatio.py
@@ -0,0 +1,66 @@
+import pandas as pd
+
+
+def golden_ratio(func, interval, pression=10 ** (-5), max_iter=500,
+                 flag_results=False, flag_data=False, flag_opt=1):
+    '''
+    Функция поиска экстремума функции одной переменной методом золотого сечения
+    :param func: python function
+    :param interval: list consisting of float numbers(left and right bounds)
+    :param pression: optimization accuracy
+    :param max_iter: maximum amount of iterations
+    :param flag_results: flag: output of intermediate results
+    :param flag_data: flag: recording intermediate results in a dataset
+    :param flag_opt: type of extrema: if min:flag_opt=1
+                                      if max: flag_opt=-1
+
+    :return: extrema:the point at which the extremum
+             df: intermediate results in a dataset
+    '''
+
+    data = []
+    df = pd.DataFrame(columns=['iter', 'point', 'function'])
+    phi = (5 ** 0.5 + 1) / 2
+    a = interval[0]
+    b = interval[1]
+    extrema = (b + a) / 2
+
+    x1 = b - (b - a) / phi
+    x2 = a + (b - a) / phi
+
+    if flag_data:
+        data = [[0, extrema, func(extrema)]]
+    if flag_results:
+        print(f"Номер итерации {0}, точка {(a+b) / 2}, функция {func((a+b) / 2)}.")
+
+    for i in range(max_iter):
+        if abs(a - b) >= pression:
+            if flag_opt*(x1) > flag_opt*(x2):
+                a = x1
+                x1 = b - (b - a) / phi
+                x2 = a + (b - a) / phi
+            else:
+                b = x2
+                x1 = b - (b - a) / phi
+                x2 = a + (b - a) / phi
+            extrema = (b + a) / 2
+
+        else:
+            break
+
+        if flag_data:
+            data.append(([i+1, extrema, func(extrema)]))
+            df = pd.DataFrame(data, columns=['iter', 'point', 'function'])
+
+        if flag_results:
+            print(f"Номер итерации {i+1}, точка {extrema}, функция {func(extrema)}.")
+
+    return extrema, df
+
+
+if __name__ == '__main__':
+    def f(x): return x ** 3 - x ** 2 - x
+
+
+    print(golden_ratio(f, [0, 1.5], pression=10 ** (-5), max_iter=500,
+                       flag_results=1, flag_data=0))
