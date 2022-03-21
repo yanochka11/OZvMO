3d662a269d9e347228622c69fe94f11f7779a9ad
adelinatsoi <dlnxxts@gmail.com>
2022-03-21 21:14:22 +0300

    решения

--- TZ2/solutions.py
+++ TZ2/solutions.py
@@ -4,9 +4,8 @@ from OZvMO.TZ2.parabols import parabolic_interpolation
 from IPython.display import display
 
 
-
 def solve_golden_ratio(precision=1e-5, max_iter=500,
-                       flag_results=False, flag_data=False, type_opt = 'min'):
+                       flag_results=False, flag_data=False, type_opt='min'):
     """
     Функция поиска экстремума функции одной переменной методом золотого сечения
     :param precision: optimization accuracy
@@ -20,14 +19,14 @@ def solve_golden_ratio(precision=1e-5, max_iter=500,
     variables, func, interval = parser_task_gr()
 
     func, interval, extrema, df = golden_ratio(func, interval, pression=precision, max_iter=max_iter,
-                               flag_results=flag_results, flag_data=flag_data, type_opt=type_opt)
+                                               flag_results=flag_results, flag_data=flag_data, type_opt=type_opt)
     if df.shape[0] != 0:
         display(df)
     print(f'точка экстремума: x = {extrema}, значение функции {func(extrema)}.')
 
 
 def solve_parabolic_interpolation(precision=1e-5, max_iter=500,
-                       flag_results=False, flag_data=False, type_opt = 'min'):
+                                  flag_results=False, flag_data=False, type_opt='min'):
     """
     Функция поиска экстремума функции одной переменной методом парабол
     :param precision: optimization accuracy
@@ -41,10 +40,11 @@ def solve_parabolic_interpolation(precision=1e-5, max_iter=500,
     variables, func, interval = parser_task_gr()
 
     extrema, df = parabolic_interpolation(func, interval, pression=precision, max_iter=max_iter,
-                               flag_results=flag_results, flag_data=flag_data, type_opt=type_opt)
+                                          flag_results=flag_results, flag_data=flag_data, type_opt=type_opt)
     if df.shape[0] != 0:
         display(df)
     print(f'точка экстремума: x = {extrema}, значение функции {func(extrema)}.')
+
 
 def solve_brent(pression=1e-5, max_iter=500,
                 flag_results=False, flag_data=False, type_opt='min'):
@@ -66,6 +66,7 @@ def solve_brent(pression=1e-5, max_iter=500,
         display(df)
     print(f'точка экстремума: x = {extrema}, значение функции {func(extrema)}.')
 
+
 if __name__ == '__main__':
     solve_parabolic_interpolation(precision=1e-5, max_iter=500,
-                       flag_results=True, flag_data=True, type_opt='min')
+                                  flag_results=True, flag_data=True, type_opt='min')
