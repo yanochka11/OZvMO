d4f8bf3bd1947c80e1262b72d83610a5fcaa5c7f
adelinatsoi <dlnxxts@gmail.com>
2022-03-22 15:26:52 +0300

    исправила display

--- TZ2/solutions.py
+++ TZ2/solutions.py
@@ -21,7 +21,7 @@ def solve_golden_ratio(precision=1e-5, max_iter=500,
     func, interval, extrema, df = golden_ratio(func, interval, pression=precision, max_iter=max_iter,
                                                flag_results=flag_results, flag_data=flag_data, type_opt=type_opt)
     if df.shape[0] != 0:
-        display(df['iter', 'point', 'function'])
+        display(df[['iter', 'point', 'function']])
     print(f'точка экстремума: x = {extrema}, значение функции {func(extrema)}.')
 
 
@@ -68,5 +68,5 @@ def solve_brent(pression=1e-5, max_iter=500,
 
 
 if __name__ == '__main__':
-    solve_parabolic_interpolation(precision=1e-5, max_iter=500,
+    solve_golden_ratio(precision=1e-5, max_iter=500,
                                   flag_results=True, flag_data=True, type_opt='min')
